13b619ae7a47000af9cfa2fdde3692eea1220edf
Show MATSUOKA <show@architect-matsuoka.jp>
2021-10-04 23:57:47 +0900

    38Lo7SnS46c : システム変数を追加
    	modified:   src/main.cxx
    	modified:   manual/manual.htm
    	modified:   manual/manual.md

--- src/main.cxx
+++ src/main.cxx
@@ -13,13 +13,40 @@
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+#ifndef TURNUP_MAJOR_VERSION
+	#define TURNUP_MAJOR_VERSION 0
+#endif //TURNUP_MAJOR_VERSION
+
+#ifndef TURNUP_MINOR_VERSION
+	#define TURNUP_MINOR_VERSION 0
+#endif //TURNUP_MINOR_VERSION
+
+#ifndef TURNUP_DRAFT_VERSION
+	#define TURNUP_DRAFT_VERSION 1
+#endif //TURNUP_DRAFT_VERSION
+
 
 using namespace turnup;
 
 
-static void ShowVersion();
-static bool InjectVariables( PreProcessor& pp, const Parameters& params );
+struct SysVars {
+	TextSpan	appName;	// __APP_NAME
+	char		appVer[8];	// __APP_VERSION
+	TextSpan	fileName;	// __FILE
+	char		date[40];	// __DATE
+	char		time[40];	// __TIME
+};
 
+
+static void ShowVersion();
+static TextSpan RemovePath( const char* pTop, const char* pEnd = nullptr );
+static void SetupSystemVariables( SysVars& sysVars, const char* pAppName );
+static void InjectSystemVariables( PreProcessor& pp, const SysVars& sysVars );
+static bool InjectParamVariables( PreProcessor& pp, const Parameters& params );
 
 //------------------------------------------------------------------------------
 //
@@ -37,6 +64,9 @@ int main( int argc, char* argv[] ) {
 		ShowVersion();
 		return 0;
 	}
+
+	SysVars sysVars;
+
 	InputData* pInData = nullptr; {
 		TextSpan fileName;
 		if( params.GetTargetFile( fileName ) == false ) {
@@ -48,12 +78,15 @@ int main( int argc, char* argv[] ) {
 			std::cerr << "aborted." << std::endl;
 			return 1;
 		}
+		sysVars.fileName = RemovePath( fileName.Top(), fileName.End() );
 	}
+
+	SetupSystemVariables( sysVars, argv[0] );
 
 	{
 		PreProcessor* pPP = PreProcessor::Create();
-		//ToDo : eiRfJCrt9ae : システム変数は将来ここで登録
-		if( InjectVariables( *pPP, params ) == false ) {
+		InjectSystemVariables( *pPP, sysVars );
+		if( InjectParamVariables( *pPP, params ) == false ) {
 			std::cerr << "aborted." << std::endl;
 			return 1;
 		}
@@ -97,19 +130,6 @@ int main( int argc, char* argv[] ) {
 //
 //------------------------------------------------------------------------------
 static void ShowVersion() {
-
-#ifndef TURNUP_MAJOR_VERSION
-	#define TURNUP_MAJOR_VERSION 0
-#endif //TURNUP_MAJOR_VERSION
-
-#ifndef TURNUP_MINOR_VERSION
-	#define TURNUP_MINOR_VERSION 0
-#endif //TURNUP_MINOR_VERSION
-
-#ifndef TURNUP_DRAFT_VERSION
-	#define TURNUP_DRAFT_VERSION 1
-#endif //TURNUP_DRAFT_VERSION
-
 	std::cout << "turnup version "
 			  << TURNUP_MAJOR_VERSION << '.'
 			  << std::setw(3) << std::setfill('0') << std::right << TURNUP_MINOR_VERSION;
@@ -120,7 +140,37 @@ static void ShowVersion() {
 
 }
 
-static bool InjectVariables( PreProcessor& pp, const Parameters& params ) {
+static TextSpan RemovePath( const char* pTop, const char* pEnd ) {
+	if( !pEnd )
+		pEnd = pTop + ::strlen( pTop );
+	while( true ) {
+		auto p = std::find( pTop, pEnd, '/' );
+		if( p == pEnd )
+			break;
+		pTop = p + 1;
+	}
+	return TextSpan{ pTop, pEnd };
+}
+
+static void SetupSystemVariables( SysVars& sysVars, const char* pAppName ) {
+	sysVars.appName = RemovePath( pAppName );
+	sprintf( sysVars.appVer, "%d.%03d", TURNUP_MAJOR_VERSION, TURNUP_MINOR_VERSION );
+	time_t now;
+	time( &now );
+	struct tm* pTM = localtime( &now );
+	sprintf( sysVars.date, "%04d/%02d/%02d", pTM->tm_year+1900, pTM->tm_mon+1, pTM->tm_mday );
+	sprintf( sysVars.time, "%02d:%02d:%02d", pTM->tm_hour, pTM->tm_min, pTM->tm_sec );
+}
+
+static void InjectSystemVariables( PreProcessor& pp, const SysVars& sysVars ) {
+	pp.RegisterVariable( TextSpan{ "__APP_NAME"    }, sysVars.appName            );
+	pp.RegisterVariable( TextSpan{ "__APP_VERSION" }, TextSpan{ sysVars.appVer } );
+	pp.RegisterVariable( TextSpan{ "__FILE"        }, sysVars.fileName           );
+	pp.RegisterVariable( TextSpan{ "__DATE"        }, TextSpan{ sysVars.date }   );
+	pp.RegisterVariable( TextSpan{ "__TIME"        }, TextSpan{ sysVars.time }   );
+}
+
+static bool InjectParamVariables( PreProcessor& pp, const Parameters& params ) {
 	bool result = true;
 	const uint32_t cnt = params.DefinitionCount();
 	for( uint32_t i = 0; i < cnt; ++i ) {
